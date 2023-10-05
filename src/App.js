856d36c07c89db5d41e5acc8e1cfcff375f4fc3b
ahmedashrafbmj <ahmedzain7557@gmail.com>
2023-10-05 14:13:22 +0500

    ahmed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [createinput, setCreateinput] = useState([]);
+
+  function create() {
+    setCreateinput([...createinput, { name: "" }]);
+  }
+
+  function deletes(e) {
+    // const newArr = createinput.filter((x,i)=> i !== e)
+    // console.log(e)
+    // console.log(createinput)
+    // createinput.splice(e,1)
+    const rows = [...createinput];
+    rows.splice(e, 1);
+
+    setCreateinput([...rows]);
+  }
+  // console.log(createinput, "createinput");
+
+  function addname(i, val) {
+    console.log(`index${i},,val${val} `);
+    const newarr = [...createinput];
+    console.log(newarr[i]);
+    const obj = newarr[i];
+    obj.name = val;
+    newarr[i] = obj;
+    setCreateinput([...newarr]);
+  }
+
+  console.log(createinput, "bef rom fun");
+
+  function RemoveVal(i) {
+    console.log(`index ${i}  `);
+    const newArr = [...createinput];
+   
+    console.log(newArr[i]);
+    const obj = newArr[i];
+    obj.name = "";
+    newArr[i] = obj;
+    setCreateinput([...newArr]);
+
+  }
+
+
+
+  
+
+  console.log(createinput);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <img src="https://www.nopcommerce.com/images/thumbs/0005720_coming-soon-page_550.jpeg" className="img-fluid"/>
+
     </div>
   );
 }
