7495c6f6cd15d26e7a5f11b2293d629c1bb73990
ahmedashrafbmj <ahmedzain7557@gmail.com>
2023-11-24 10:36:49 +0500

    ahmed

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
  
   return (
     <div className="App">
-     <img src="https://www.nopcommerce.com/images/thumbs/0005720_coming-soon-page_550.jpeg" className="img-fluid"/>
+     <img src="https://www.nopcommerce.com/images/thumbs/0005720_coming-soon-page_550.jpeg" className="img-fluid" alt="ahmed"/>
 
     </div>
   );
